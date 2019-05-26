490b9e043b72e67fdfd751d1d4136492b228f177
YunzhengZhou <zhouy36@mymacewan.ca>
2019-05-26 08:49:47 -0400

    add all existed client-website scripts

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+// get path from built-in module
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['./starter/src/js/index.js', 'babel-polyfill'],
+    output: {
+        path: path.resolve(__dirname, 'starter/dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './starter/dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './starter/src/index.html'
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test:/\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+};
+
